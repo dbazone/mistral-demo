79c3cd8cf3aa6e3eb42dc035482cec1e883c7693
Vijay Mishra <dbazone@gmail.com>
2024-06-17 19:06:58 +0530

    much complex prompt with detailed output

--- basic.py
+++ basic.py
@@ -7,6 +7,14 @@ from mistral_common.protocol.instruct.messages import UserMessage
 from mistral_common.protocol.instruct.request import ChatCompletionRequest
 import os
 
+prompt = """I would like you to build the story with theme of 'Adventures of superman in India'. 
+The story should be targeted for teenagers and should have elements of engagement from both boys and girls as target audience. 
+
+The story timeline should be starting back to 3000 years back to present day and should include the encounter of superman with famous historical and current personalities including Gautam Budha, Ashoka, Chankya Guru Nanak, Shivaji Maharaj, Viveknanda, APJ Abdul kalam. 
+
+The story should be based on Superman perspective
+"""
+
 accessToken = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 mistralModelsPath = Path.home().joinpath('mistral_models', '7B-Instruct-v0.3')
 mistralModelsPath.mkdir(parents=True, exist_ok=True)
@@ -15,8 +23,8 @@ snapshot_download(repo_id="mistralai/Mistral-7B-Instruct-v0.3", allow_patterns=[
 tokenizer = MistralTokenizer.from_file(f"{mistralModelsPath}/tokenizer.model.v3")
 model = Transformer.from_folder(mistralModelsPath)
 
-completion_request = ChatCompletionRequest(messages=[UserMessage(content="Explain Machine Learning to me in a nutshell.")])
+completion_request = ChatCompletionRequest(messages=[UserMessage(content=prompt)])
 tokens = tokenizer.encode_chat_completion(completion_request).tokens
-out_tokens, _ = generate([tokens], model, max_tokens=64, temperature=0.0, eos_id=tokenizer.instruct_tokenizer.tokenizer.eos_id)
+out_tokens, _ = generate([tokens], model, max_tokens=4096, temperature=0.0, eos_id=tokenizer.instruct_tokenizer.tokenizer.eos_id )
 result = tokenizer.instruct_tokenizer.tokenizer.decode(out_tokens[0])
 print(result)
